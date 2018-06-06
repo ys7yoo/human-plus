6a324c6c3a7fbb6546f3a2f4b83dcc6d2c11c4d0
YY <ys7yoo@gmail.com>
2018-06-06 12:17:31 +0900

    cwt code

--- cwt_analysis.m
+++ cwt_analysis.m
@@ -1,14 +1,16 @@
-function WdB = cwt_analysis(iEEG)
+function WTdB = cwt_analysis(iEEG, fs, f)
 
-num_ch = 42; % channel of interest 17 or 18
-f=[1:12 13:2:30 32:4:100]; % or, freq = 100;
-sfreq = 1600;
+%num_ch = 42; % channel of interest 17 or 18
+%f=[1:12 13:2:30 32:4:100]; % or, freq = 100;
+%sfreq = 1600;
+
+num_ch = size(iEEG,3);
 
 for chans=1:num_ch % channel number
     for freq=1:length(f) %frequency
         for j=1:size(iEEG,3)
-            wt = cwt(squeeze(iEEG(chans,:,j)),sfreq./(freq),'cmor2.48-1'); % cwt with 'cmor'
-            WdB(chans,freq,:,j) = 20*log10(abs(wt)); %cwt_filt_data(chans, freq,timepoints); power (dB)
+            wt = cwt(squeeze(iEEG(chans,:,j)), fs./freq, 'cmor2.48-1'); % cwt with 'cmor'
+            WTdB(chans,freq,:,j) = 20*log10(abs(wt)); %cwt_filt_data(chans, freq,timepoints); power (dB)
             %energy(freq,1) = sqrt(sum(abs(cdata).^2));
         end
     end

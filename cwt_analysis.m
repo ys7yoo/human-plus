ffbf22a341c6d1e569d8c7ed3cee50566b0d93fc
YY <ys7yoo@gmail.com>
2018-06-06 12:36:26 +0900

    fixed bug in index

--- cwt_analysis.m
+++ cwt_analysis.m
@@ -4,11 +4,11 @@ function WTdB = cwt_analysis(iEEG, fs, f)
 %f=[1:12 13:2:30 32:4:100]; % or, freq = 100;
 %sfreq = 1600;
 
-num_ch = size(iEEG,3);
+[num_ch, ~, numTrial]= size(iEEG);
 
 for chans=1:num_ch % channel number
     for freq=1:length(f) %frequency
-        for j=1:size(iEEG,3)
+        for j=1:numTrial
             wt = cwt(squeeze(iEEG(chans,:,j)), fs./freq, 'cmor2.48-1'); % cwt with 'cmor'
             WTdB(chans,freq,:,j) = 20*log10(abs(wt)); %cwt_filt_data(chans, freq,timepoints); power (dB)
             %energy(freq,1) = sqrt(sum(abs(cdata).^2));
